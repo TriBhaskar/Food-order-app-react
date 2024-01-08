4b0debee7e40193e864396c7d490eb9f593a8bd0
TriBhaskar <panthribhaskar@gmail.com>
2024-01-08 22:25:33 +0530

    Fix bug in cartReducer

--- src/store/CartContext.jsx
+++ src/store/CartContext.jsx
@@ -25,9 +25,9 @@ function cartReducer(state, action) {
     return { ...state, items: updatedItems };
   }
   if (action.type === "REMOVE_ITEM") {
-    const existingCartItemIndex = state.items.findIndex((item) => {
-      item.id === action.id;
-    });
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.id
+    );
     const existingCartItem = state.items[existingCartItemIndex];
     const updatedItems = [...state.items];
     if (existingCartItem.quantity === 1) {
