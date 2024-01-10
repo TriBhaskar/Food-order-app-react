6eeb4818012e91610b052c2bae0465942551d674
TriBhaskar <panthribhaskar@gmail.com>
2024-01-10 21:12:58 +0530

    Refactor code for consistency and fix order validation bug

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -5,6 +5,7 @@ import { currencyFormatter } from "../util/formatting.js";
 import Input from "./UI/Input.jsx";
 import Button from "./UI/Button.jsx";
 import UserProgressContext from "../store/UserProgressContext.jsx";
+import axios from "axios";
 
 export default function Checkout() {
   const cartCtx = useContext(CartContext);
@@ -17,14 +18,34 @@ export default function Checkout() {
   function handleClose() {
     userProgressCtx.hideCheckout();
   }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData.entries()); // { fullName: "John Doe", email: "..." }
+    console.log(data);
+    console.log(cartCtx.items);
+    axios
+      .post("http://localhost:3000/orders", {
+        order: {
+          items: cartCtx.items,
+          customer: data,
+        },
+      })
+      .then((response) => {
+        console.log(response);
+        //   cartCtx.clearCart();
+        //   userProgressCtx.hideCheckout();
+      });
+  }
   return (
     <Modal open={userProgressCtx.progress === "checkout"} onClose={handleClose}>
-      <form action="">
+      <form action="" onSubmit={handleSubmit}>
         <h2>Checkout</h2>
         <p>Total Amount: {currencyFormatter.format(cartTotal)}</p>
-        <Input label="Full Name" type="text" id="full-name" />
+        <Input label="Full Name" type="text" id="name" />
         <Input label="Email Address" type="email" id="email" />
-        <Input label="Street" type="text" id="Street" />
+        <Input label="Street" type="text" id="street" />
 
         <div className="control-row">
           <Input label="Postal Code" type="text" id="postal-code" />
